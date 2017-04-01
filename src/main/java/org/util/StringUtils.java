66f3e834225915b6ae810c78f2f1a976be3fff14
terrence <tzh@yjcit.com>
2017-04-01 11:22:36 +0800

    字符串工具类

---
+++ src/main/java/org/util/StringUtils.java
@@ -0,0 +1,24 @@
+package org.util;
+
+/**
+ * @author Terrence
+ */
+public class StringUtils {
+
+    public static boolean hasLength(CharSequence str) {
+        return (str != null && str.length() > 0);
+    }
+
+    public static boolean hasText(CharSequence str) {
+        if (!hasLength(str)) {
+            return false;
+        }
+        int strLen = str.length();
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
